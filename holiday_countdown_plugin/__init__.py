f9e7a85bd922b3b5c91a901aba141646970b84a4
Kevin Shipley <kevinashipley@gmail.com>
2025-09-15 15:50:23 -0400

    starting holiday countdown board

---
+++ holiday_countdown_plugin/__init__.py
@@ -0,0 +1,18 @@
+"""
+The **Holiday Countdown Board** displays upcoming holidays on an LED matrix with customizable colors and images.
+"""
+
+# Board metadata using standard Python package conventions
+__version__ = "2025.09.01"
+__description__ = "Holiday Countdown Board displays upcoming holidays with customizable themes"
+__board_name__ = "Holiday Countdown Board"
+__author__ = "kas"
+
+# Board requirements (optional)
+__requirements__ = [
+    "pillow",
+    "holidays"
+]
+
+# Minimum application version required (optional)
+__min_app_version__ = "2025.09.00"
